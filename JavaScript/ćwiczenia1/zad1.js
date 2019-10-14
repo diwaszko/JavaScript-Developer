6cae5782f02e4e695b39ca72ff3cadd364f912f6
Daniel Iwaszko <pepiozo@mail.com>
2019-10-14 22:36:01 +0200

    Added zad1.js

---
+++ JavaScript/ćwiczenia1/zad1.js
@@ -0,0 +1,9 @@
+// From years in array check for leap years [1974, 1900, 1985, 2000]
+
+let years = [1974, 1900, 1985, 2000, 2004];
+
+for ( i = 0; i < years.length; i++ ){
+    if ((years[i] % 4 == 0 && years[i] % 100 != 0) || (years[i] % 400 == 0)){
+        console.log(years[i]);
+    } 
+};
