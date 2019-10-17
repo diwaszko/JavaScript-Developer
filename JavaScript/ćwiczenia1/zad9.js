6057de03dcab57e819765592077b681337679eea
Daniel Iwaszko <pepiozo@mail.com>
2019-10-17 23:31:56 +0200

    Added zad9.js

---
+++ JavaScript/ćwiczenia1/zad9.js
@@ -0,0 +1,15 @@
+// 9) With a given start value of 0. Iterate the array and add even items and subtract odd ones. [1,6,23,8,4,98,3,7,3,98,4,98]
+
+
+let items = [1,6,23,8,4,98,3,7,3,98,4,98];
+let value = 0;
+
+for ( i = 0; i < items.length; i++ ){
+    if ( items[i] % 2 == 0 ){
+        value = value + items[i];
+    }
+    else{
+        value = value - items[i];       
+    }
+};
+console.log('Result: ' + value);
