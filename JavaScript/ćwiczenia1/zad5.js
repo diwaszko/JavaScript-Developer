4544661ef6eb633c9a1ed4ad25fbfe969b6b0b87
Daniel Iwaszko <pepiozo@mail.com>
2019-10-17 22:15:53 +0200

    Added zad5.js

---
+++ JavaScript/ćwiczenia1/zad5.js
@@ -0,0 +1,11 @@
+// 5)	Choose longest string from the array. ['Karol', 'Adam','Rogowski','Politechnika','Super','Weekend']. 
+
+let items = ['Karol', 'Adam', 'Rogowski', 'Politechnika', 'Super', 'Weekend'];
+let longest = items[0];
+
+for ( i = 0; i < items.length; i++ ){
+    if ( items[i].length >= longest.length ){
+        longest = items[i];
+    }
+};
+console.log('Longest is: ' + longest);
