df356b67d8977e36691b5fe1d9a82389ef7cccbf
Daniel Iwaszko -lap <pepiozo@mail.com>
2019-10-28 19:28:34 +0100

    Added lessons note

---
+++ JavaScript/cwiczenia3/note.js
@@ -0,0 +1,98 @@
+// 8 zadanie jest to zadanie egzaminacyjne (omawiane na ostatnim zjeździe)
+// porównac trzeba 3 pierwsze elementy z 3 nastepnymi (jezeli sa rowne to wiadomo, ze w 3 grupie jest kulka o wartosci 2) 
+
+function dwa() {                          
+    console.log(3);
+};  
+
+dwa();
+
+//IIFE - natychmiastowo wykonana funkcja ()  - sostuje się np do obektów konfiguracyjnych, tylko 1 raz
+(function() {                           // aby wywołać funkcję natychmiastowo należy opakować funkcje w ()
+    console.log(2);
+})();                                   // (); na końcu wywołuje od razu funkcję, ale tylko w przypadku, gdy jest                                                  potrzebna jeden raz
+dwa();
+
+
+//  wzorzec projektowy "closure" - pozwala na ograniczenie dostępu do nietkorych elementów i ich zmiany (na poziomie wzora kodu, a nie bezpieczeństwa - nie zabezpiecza przed wyciekiem informacji jak np. hasła)
+
+// IIFE i closure PRZETESTOWAC W DOMU!!!!!!!!!!
+
+// słowo kluczowe this
+console.log('-----------------');
+
+(function() {
+    console.log(this);
+})();
+
+
+
+let id = 3;
+
+let obj = {
+    id: 1,
+    getThisId: function(){
+        let id = 2;
+        return this.id;
+    },
+    getId: function(){
+        let id = 2;
+        return id;
+    },
+    getOuterId: function(){
+        return id;
+    }
+};
+console.log(obj.getThisId());   // zwróci 1, bo this odnosi się do obiektu tak jak: obj.id
+console.log(obj.getId());       // zwróci 2, bo zwraca zmienną ze scope funkcji getId
+console.log(obj.getOuterId());  // zwróci 3, bo nie ma wartości w scope funkcji, ale jest globalnie przed                                              utworzeniem obiektu
+
+
+// metoda "call" jest to działanie na funkcji (nie polega na wywołaniu funkcji)
+
+console.log('-----------------');
+function thisExample(){
+    console.log(this);
+}
+
+let obj1 = { x: 1};
+let obj2 = {};
+let obj3 = { arr: [1,2,3]};
+
+thisExample.call(obj1);
+thisExample.call(obj2);
+thisExample.call(obj3);
+
+console.log('-----------------');
+
+let obj10 = {
+    id: 1,
+    getId: function(){
+        return this.id;
+    }
+};
+let contextObject = { id: 2 };
+console.log(obj10.getId());                             // POCZYTAĆ !!!!
+console.log(obj10.getId.call(contextObject));           // wywołanie z obiektu obj10 wartości obiektu contextObject                                                             - natychmiastowo wywołuje funkcje
+console.log(obj10.getId.apply(contextObject));           //  Funkcja apply ?? - natychmiastowo wywołuje funkcje
+
+console.log('----------------- bind');
+
+let newGetId = obj10.getId.bind(contextObject);         //  Funkcja bind - przypisuje funkcje getId
+
+console.log(obj10.getId());           
+console.log(newGetId());           
+
+console.log('-----------------');
+
+
+// Parametry w funkcji
+
+let showInfo = function(main, prefix = "P", sufix = "S"){
+    console.log(prefix,main,sufix);
+}
+
+showInfo();
+showInfo("example");
+showInfo("example", undefined, "My Sufix");
+showInfo("example", "My Prefix", "My Sufix");
