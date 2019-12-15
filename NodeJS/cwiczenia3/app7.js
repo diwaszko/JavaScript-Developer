cff5c8b6ae44737f203767497c9509c435516692
Daniel Iwaszko -lap <pepiozo@mail.com>
2019-12-15 15:17:24 +0100

    fix

--- NodeJS/cwiczenia3/app7.js
+++ NodeJS/cwiczenia3/app7.js
@@ -3,6 +3,9 @@
 // https://api.openweathermap.org/data/2.5/weather?appid=0ed761300a2725ca778c07831ae64d6e&lat={LAT}&lon={LNG}
 // gdzie `{LAT}` i `{LNG}` to współrzędne geograficzne pobrane od naszego użytkownika
 
+
+
+// Trzeba podzielić pobieranie pogody i info o użytkowniku na 2 oddzielne części - dostępne będzie na gitcie
 const request = require("request");
 const argv = require("yargs").argv;
 
@@ -22,7 +25,7 @@ request(
       return;
     }
     if (res.statusCode === 200) {
-      
+
       const data = JSON.parse(body);
       const userName = data.name;
       const userLat = data.address.geo.lat;
@@ -32,26 +35,26 @@ request(
       console.log("Lat:", userLat);
       console.log("Lng:", userLng);
 
-    const wetaherUrl = `https://api.openweathermap.org/data/2.5/weather?appid=0ed761300a2725ca778c07831ae64d6e&lat=${userLat}&lon=${userLng}`;
-    request(
-      wetaherUrl, 
-      (err, res, body) => {
-        if (err) {
-          console.log("Server connection error.");
-          return;
+      const wetaherUrl = `https://api.openweathermap.org/data/2.5/weather?appid=0ed761300a2725ca778c07831ae64d6e&lat=${userLat}&lon=${userLng}`;
+      request(
+        wetaherUrl,
+        (err, res, body) => {
+          if (err) {
+            console.log("Server connection error.");
+            return;
+          }
+          if (res.statusCode === 404) {
+            console.log(`Weather for ${userName} not found.`);
+            return;
+          }
+          if (res.statusCode != 200) {
+            console.log(`Data not found. Status code: ${res.statusCode}`);
+            return;
+          }
+          const weatherInfo = JSON.parse(body);
+          console.log(weatherInfo);
         }
-        if (res.statusCode === 404) {
-          console.log(`Weather for ${userName} not found.`);
-          return;
-        }
-        if (res.statusCode != 200) {
-          console.log(`Data not found. Status code: ${res.statusCode}`);
-          return;
-        }
-        const weatherInfo = JSON.parse(body);
-        console.log(weatherInfo);
-      }
-    )
+      )
     }
   }
 );
